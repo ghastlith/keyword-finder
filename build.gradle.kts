1a9ea3e5a3030709f457a286f5946694ebbfd853
ghastlith <72772611+ghastlith@users.noreply.github.com>
2026-04-27 18:53:51 -0300

    refactor: change kts file tabs to match industry standards

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,46 @@
 plugins {
-    java
-    id("org.springframework.boot") version "3.5.4"
-    id("io.spring.dependency-management") version "1.1.7"
+  java
+  id("org.springframework.boot") version "3.5.4"
+  id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "ghastlith"
 version = "3.0.0"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+  }
 }
 
 dependencies {
-    // spring
-    implementation("org.springframework.boot:spring-boot-starter-web:3.5.4")
-    implementation("org.springframework.boot:spring-boot-starter-validation:3.5.4")
+  // spring
+  implementation("org.springframework.boot:spring-boot-starter-web:3.5.4")
+  implementation("org.springframework.boot:spring-boot-starter-validation:3.5.4")
 
-    // tests
-    testImplementation("org.springframework.boot:spring-boot-starter-test:3.5.4")
-    testImplementation("org.assertj:assertj-core:3.24.2")
-    testImplementation("org.mockito:mockito-core:5.19.0")
-    testImplementation("org.mockito:mockito-junit-jupiter:5.19.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.13.4")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.13.4")
+  // tests
+  testImplementation("org.springframework.boot:spring-boot-starter-test:3.5.4")
+  testImplementation("org.assertj:assertj-core:3.24.2")
+  testImplementation("org.mockito:mockito-core:5.19.0")
+  testImplementation("org.mockito:mockito-junit-jupiter:5.19.0")
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.13.4")
+  testImplementation("org.junit.jupiter:junit-jupiter-params:5.13.4")
 
-    // lombok
-    compileOnly("org.projectlombok:lombok:1.18.38")
-    annotationProcessor("org.projectlombok:lombok:1.18.38")
-    testCompileOnly("org.projectlombok:lombok:1.18.38")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.38")
+  // lombok
+  compileOnly("org.projectlombok:lombok:1.18.38")
+  annotationProcessor("org.projectlombok:lombok:1.18.38")
+  testCompileOnly("org.projectlombok:lombok:1.18.38")
+  testAnnotationProcessor("org.projectlombok:lombok:1.18.38")
 }
 
 tasks.withType<Test>().configureEach {
-    useJUnitPlatform()
+  useJUnitPlatform()
 
-    testLogging {
-        events("passed", "skipped", "failed")
-    }
+  testLogging {
+    events("passed", "skipped", "failed")
+  }
 }
